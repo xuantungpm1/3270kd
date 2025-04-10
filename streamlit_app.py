a92646734c1bea86cdf315bb3ef84aac3a3ea18b
xuantungpm1 <xuantungpm1@gmail.com>
2025-04-10 10:00:34 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,8 +67,40 @@ if input_id:
             paper_bgcolor="#2c3e50",  # optional dark background
             font=dict(color="white")
         )
-
+        st.title("DKP rate")
         st.plotly_chart(fig, use_container_width=False)
+
+        # Example DKP value
+        deads_rate = row['Deads rate']  # Replace with your actual value
+
+        fig1 = go.Figure(go.Indicator(
+            mode="gauge+number",
+            value=deads_rate,
+            title={'text': "DKP rate"},
+            gauge={
+                'axis': {'range': [0, 100]},
+                'bar': {'color': "#00CC96"},
+                'bgcolor': "darkgray",
+                'borderwidth': 1,
+                'bordercolor': "gray",
+                'steps': [
+                    {'range': [0, 50], 'color': "#FF4B4B"},
+                    {'range': [50, 80], 'color': "#FFA500"},
+                    {'range': [80, 100], 'color': "#00CC96"}
+                ],
+            }
+        ))
+
+        fig1.update_layout(
+            width=200,  # or any size in pixels
+            height=100,
+            margin=dict(t=0, b=0, l=0, r=0),
+            paper_bgcolor="#2c3e50",  # optional dark background
+            font=dict(color="white")
+        )
+
+        st.title("Deads rate")
+        st.plotly_chart(fig1, use_container_width=False)
     else:
         st.warning("❌ No matching ID found.")
 
